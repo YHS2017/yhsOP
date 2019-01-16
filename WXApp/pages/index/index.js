04c3b4febc87ed785c9b38c4a03d34f6cf32af95
yuan <594726016@qq.com>
2019-01-16 19:32:59 +0800

    新增微信小程序

---
+++ WXApp/pages/index/index.js
@@ -0,0 +1,20 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      'https://gss0.bdstatic.com/5bVWsj_p_tVS5dKfpU_Y_D3/res/r/image/2018-10-10/6b5efbcbe8335a452061fae6fb79a17c.jpg',
+      'https://gss0.bdstatic.com/5bVWsj_p_tVS5dKfpU_Y_D3/res/r/image/2018-10-10/6b5efbcbe8335a452061fae6fb79a17c.jpg',
+      'https://gss0.bdstatic.com/5bVWsj_p_tVS5dKfpU_Y_D3/res/r/image/2018-10-10/6b5efbcbe8335a452061fae6fb79a17c.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 500
+  },
+  onLoad: function () {
+
+  }
+})
