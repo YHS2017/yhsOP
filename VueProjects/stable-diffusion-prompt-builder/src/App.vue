6f4a0cdc6e11324fe10cac3dccc9c6bd200c4b37
YHS2017 <594726016@qq.com>
2023-03-20 13:56:17 +0800

    新增stable-diffusion-prompt-builder

---
+++ VueProjects/stable-diffusion-prompt-builder/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <a-layout>
+    <a-layout-header class="header">
+      <h1>Prompt Builder V1.0</h1>
+    </a-layout-header>
+    <a-layout-content>
+      <a-card title="已选TAG"></a-card>
+      <a-card title="prompts"></a-card>
+    </a-layout-content>
+    <a-layout-footer class="footer">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<style scoped>
+.header {
+  background-color: dodgerblue;
+}
+
+.header h1 {
+  color: #fff;
+}
+
+.footer {
+  text-align: center;
+}
+</style>
---
+++ VueProjects/stable-diffusion-prompt-builder/src/constants/index.ts
@@ -0,0 +1,330 @@
+export enum tagtypes {
+  'QUALITY' = 0,     // 质量
+  'PERSON' = 1,      // 人物
+  'ANIMAL' = 2,      // 动物
+  'ENV' = 3,         // 环境
+  'STYLE' = 4,       // 风格
+}
+
+export const tags = [
+  {
+    type: tagtypes.QUALITY,
+    text: '虚幻引擎(3D)',
+    value: 'unreal engine'
+  },
+  {
+    type: tagtypes.QUALITY,
+    text: '提高分辨率',
+    value: 'sharp focus'
+  },
+  {
+    type: tagtypes.QUALITY,
+    text: '高清',
+    value: '4k'
+  },
+  {
+    type: tagtypes.QUALITY,
+    text: '超高清',
+    value: '8k'
+  },
+  {
+    type: tagtypes.QUALITY,
+    text: 'Vary渲染',
+    value: 'vray'
+  },
+  {
+    type: tagtypes.QUALITY,
+    text: '最佳画质',
+    value: 'best quality'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '女孩',
+    value: 'girl'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '男孩',
+    value: 'boy'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '小女孩',
+    value: 'little girl'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '小男孩',
+    value: 'little boy'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '女人',
+    value: 'woman'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '男人',
+    value: 'man'
+  },
+  {
+    type: tagtypes.QUALITY,
+    text: '单人',
+    value: 'solo'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '2个女孩',
+    value: '2girl'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '2个男孩',
+    value: '2boy'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '一个男孩和一个女孩',
+    value: 'a boy and a girl'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '一男一女',
+    value: 'a man and a woman'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '正太',
+    value: 'shota'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '萝莉',
+    value: 'loli'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '辣妹',
+    value: 'gyaru'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '姐妹',
+    value: 'sisters'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兄弟',
+    value: 'brother'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '双胞胎姐妹',
+    value: 'twin sister'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '双胞胎兄弟',
+    value: 'twin brother'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '双胞胎',
+    value: 'twins'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '学生',
+    value: 'student'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '空姐',
+    value: 'stewardess'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '女服务员',
+    value: 'waitress'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '护士',
+    value: 'nurse'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '美人鱼',
+    value: 'mermaid'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '魔法少女',
+    value: 'magical girl'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '偶像',
+    value: 'idol'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '天使',
+    value: 'angel'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '魔鬼',
+    value: 'devil'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '怪兽',
+    value: 'monster'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '玩偶',
+    value: 'doll'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '小妖精',
+    value: 'elf'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '小精灵',
+    value: 'fairy'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽人',
+    value: 'orc'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '女巫',
+    value: 'witch'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '巫女',
+    value: 'miko'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '吸血鬼',
+    value: 'vampire'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+  {
+    type: tagtypes.PERSON,
+    text: '兽耳模式',
+    value: 'animal ears'
+  },
+];
